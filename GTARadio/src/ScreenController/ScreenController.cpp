b5fc5503083b696f6b6f8f28c5160fc6cbf0f6d9
HubbleCommand <39958198+HubbleCommand@users.noreply.github.com>
2021-05-27 16:10:30 +0200

    Screen Controller Imp clean

--- GTARadio/src/ScreenController/ScreenController.cpp
+++ GTARadio/src/ScreenController/ScreenController.cpp
@@ -6,24 +6,25 @@
 ScreenController::ScreenController(uint8_t addr, int x, int y){
     this->x = x;
     this->y = y;
-    this->lcd = new LiquidCrystal_I2C(addr, x, y);
+    this->lcd = new LiquidCrystal_I2C(addr, x, y);  //INGORE INTELLISENSE, it works...
 }
 
 void ScreenController::setup(){
-    lcd->init();
-    lcd->backlight();
-    lcd->setCursor(0,0);
-    lcd->print("BOO");
+    this->lcd->init();
+    this->lcd->backlight();
+    this->lcd->setCursor(0,0);
+    this->lcd->print("BOO");
+    this->lcd->scrollDisplayRight();
 }
 
 void ScreenController::setLine(int lineNo, String text){
-    //this->lcd->init();
     this->lcd->setCursor(0, lineNo);
     //this->lcd->print("                    "); //THIS LINE CAUSES TROUBLE; JUST USE CLEAR!
     this->lcd->print(text);
 }
 
 void ScreenController::update(){
+    this->lcd->clear();
     for(int i = 0; i < 2; i++){
         this->lcd->setCursor(0,i);
         this->lcd->print(this->text[i]);
