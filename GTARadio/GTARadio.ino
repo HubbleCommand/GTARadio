4467f8d1171d7af5f613335e6498c8d45d21a74f
HubbleCommand <39958198+HubbleCommand@users.noreply.github.com>
2021-05-29 21:19:54 +0200

    Main assorted changes

--- GTARadio/GTARadio.ino
+++ GTARadio/GTARadio.ino
@@ -7,21 +7,40 @@
 #include "src/StationAbstract/StationAbstract.h"
 #include "src/StationSplit/StationSplit.h"
 #include "src/StationUnsplit/StationUnsplit.h"
-
+#include "src/StationTalkshow/StationTalkshow.h"
+#include <Wire.h>
+#include <SPI.h>
+#include <SD.h>
 #include <TMRpcm.h>
 #define SD_ChipSelectPin 4
 
 TMRpcm tmrpcm;
 
-Potentiometer volumeController = Potentiometer(A2, 10);
+Potentiometer volumeController = Potentiometer(A0, 10);
+
+/*
+StationSplit CLASSIC_ROCK = StationSplit((char*)"", (char*)"");
+StationSplit CLASSIC_ROCK = StationSplit((char*)"Classic Rock", (char*)"01_CROCK");
+StationSplit NON_STOP_POP = StationSplit((char*)"Non Stop Pop", (char*)"02_POP");
+StationSplit RADIO_LOS_SANTOS = StationSplit((char*)"Radio Los Santos", (char*)"03_HH_N");
+StationSplit CHANNEL_X = StationSplit((char*)"Channel X", (char*)"04_PUNK");
+StationTalkshow WCTR = StationTalkshow((char*)"West Coast Talk Radio", (char*)"05_T_01");
+StationSplit REBEL_RADIO = StationSplit((char*)"Rebel Radio", (char*)"06_CUNT");
+StationSplit CLASSIC_ROCK = StationSplit((char*)"", (char*)"");
+StationSplit CLASSIC_ROCK = StationSplit((char*)"", (char*)"");
+StationSplit CLASSIC_ROCK = StationSplit((char*)"", (char*)"");
+StationSplit CLASSIC_ROCK = StationSplit((char*)"", (char*)"");
+StationSplit CLASSIC_ROCK = StationSplit((char*)"", (char*)"");
+StationSplit CLASSIC_ROCK = StationSplit((char*)"", (char*)"");
+StationSplit CLASSIC_ROCK = StationSplit((char*)"", (char*)"");
+StationSplit CLASSIC_ROCK = StationSplit((char*)"", (char*)"");*/
 
 //StationAbstract station = StationSplit((char*)"AA", (char*)"BB");
 //AbstractStation station1 = StationSplit((char*)"AA", (char*)"BB");
 //AbstractStation stations[] = {station1};
 //StationController stationController = StationController(stations);
 
-//ScreenController screen = ScreenController(0x3f,16,2);
-ScreenController screen = ScreenController(0x3f,16,2);
+ScreenController screen = ScreenController(0x27,16,2);    //0x27 for 20x4, 0x3f for 16x2
 Button buttonA = Button(2);
 Button buttonB = Button(3);
 Button buttonC = Button(7);
@@ -44,7 +63,7 @@ void setup()
 
     tmrpcm.speakerPin = 9;
     tmrpcm.volume(0.5);
-    tmrpcm.play((char*)"WAV2/WAV/WAV/WAV/WAV/1.wav");
+    tmrpcm.play((char*)"CROATTSS.wav");
 }
 
 void loop()
@@ -68,6 +87,6 @@ void loop()
 
     if(volumeController.stateChanged()){
         int volume = volumeController.getState();
-        screen.setLine(1, String(volume));
+        screen.setLine(1, String(map(volume, 0, 1023, 0, 7)));
     }
 }
