9880b3f27fb0d611116487553f1445a5e2712c96
HubbleCommand <39958198+HubbleCommand@users.noreply.github.com>
2021-06-03 22:43:04 +0200

    Main no VLA strings, works!

--- GTARadio/GTARadio.ino
+++ GTARadio/GTARadio.ino
@@ -15,16 +15,25 @@ const char* src0 = "/SRC.wav";
 const int advCount PROGMEM = 96;
 const int newsCount PROGMEM = 107;
 
+//This is some ungodly code that could be made better, 
+
 TMRpcm tmrpcm;
 ScreenController screen = ScreenController(0x27,20,4);    //0x27 for 20x4, 0x3f for 16x2
 Button butNextStation = Button(2);
 Button butPrevStation = Button(3);
 
+//Defines data for each station
 struct Station {
     const char source [9]; //While it can be smaller, this is easy
     const char* name;
     char type; //0 - Unsplit, 1 - Split, 2 - Talkshow   //char is used instead of int as it is 1 instead of 2 bytes
     char songCount;
+};
+
+//Stores the song & time reached for the song for a station
+struct StationPosition{
+    char songID;    //Playing song ID (the number)
+    int secs;     //The current time in seconds that the song last got to
 };
 
 //Storing these strings takes a lot of space, so put them in FLASH instead of SRAM. No other memory management to do as it is flash and is reset on each powerup.
@@ -56,7 +65,8 @@ int lastPotState;
 
 void setup()
 {
-    randomSeed(analogRead(A0));
+    //randomSeed(analogRead(A0));
+    randomSeed(analogRead(A0) / analogRead(A1) + 1);
 
     screen.setup();
     butNextStation.setup();
@@ -74,29 +84,57 @@ void setup()
     screen.setLine(0, "GTA Radio");
 }
 
+void playAd(){
+    char nameAd[13];
+    int selAd = random(96); //There are a total of 96 ads, so generate a number to get
+    sprintf(nameAd, "ADS/%i.wav", selAd);
+    tmrpcm.play(nameAd);
+}
+
+void playNews(){
+    char nameNews[14];
+    int selNews = random(107); //There are a total of 107 news lines, so generate a number to get
+    sprintf(nameNews, "NEWS/%i.wav", selNews);
+    tmrpcm.play(nameNews);
+}
+
 void startType0(Station station){   //Unsplit
     screen.setLine(1, (char*)station.name);
 
-    char* name_with_extension;
-    name_with_extension = (char*) malloc(strlen(station.source)+1+8);
-    strcpy(name_with_extension, station.source); 
-    strcat(name_with_extension, src0);
-    tmrpcm.play(name_with_extension, millis() / 1000);
+    /*char* nameUnsplit;
+    nameUnsplit = (char*) malloc(9+1+9);
+    strcpy(nameUnsplit, station.source); 
+    strcat(nameUnsplit, src0);
+    tmrpcm.play(nameUnsplit, millis() / 1000);
     tmrpcm.loop(1);
-    free(name_with_extension);
+    free(nameUnsplit);*/
+
+    char nameUnsplit[20];
+    strcpy(nameUnsplit, station.source); 
+    strcat(nameUnsplit, src0);
+    tmrpcm.play(nameUnsplit, millis() / 1000);
+    tmrpcm.loop(1);
 }
 
 void startType1(Station station){   //Split
-    screen.setLine(1, (char*)station.name);
+    //screen.setLine(1, (char*)station.name);
+    screen.setLine(1, station.name);
 
-    int selSong = random((int)station.songCount);
+    //int selSong = random((int)station.songCount);
+    //int selSong = random(station.songCount);
+    int selSong = random(station.songCount - '0');
+    //int selSong = 0;
 
-    char* namelov;
-    namelov = (char*) malloc(strlen(station.source)+1+14);
-    strcpy(namelov, station.source); 
+    //char* namelov;
+    char namelov[22];
+   // namelov = (char*) malloc(strlen(station.source)+1+14);
+//    namelov = (char*) malloc(9+1+14);
+    //namelov = (char*) malloc(9+1+17);
+    strcpy(namelov, station.source);
     
 
-    char* tString = new char[13];
+    //char* tString = new char[15];
+    char tString[14];
     sprintf(tString, "/SONGS/%i.wav", selSong);
 
     strcat(namelov, tString);
@@ -104,8 +142,8 @@ void startType1(Station station){   //Split
 
     tmrpcm.play(namelov);
 
-    free(namelov);
-    free(tString);
+    //free(namelov);
+    //free(tString);
 
     //screen.setLine(2, "TODO");
 }
@@ -114,8 +152,8 @@ void startType2(Station station){   //Talkshow
     screen.setLine(1, (char*)station.name);
 
     //TODO check if int can even work, can SERIOUSLY bug!
-    //int selectedTalk = random((int)station.songCount);   //Both talkshow stations only have 4 monos, so good
-    int selectedTalk = random(4);   //Both talkshow stations only have 4 monos, so good
+    //int selectedTalk = random(station.songCount);   //Both talkshow stations only have 4 monos, so good
+    /*int selectedTalk = random(4);   //Both talkshow stations only have 4 monos, so good
 
     char* name_with_extension;
     name_with_extension = (char*) malloc(strlen(station.source)+1+10);
@@ -128,6 +166,7 @@ void startType2(Station station){   //Talkshow
     strcat(name_with_extension, tmpString);
 
     tmrpcm.play(name_with_extension, millis() / 1000);
+    screen.setLine(2, name_with_extension);
 
     //TODO printing info causes a crash
     //screen.setLine(2, name_with_extension);
@@ -138,11 +177,26 @@ void startType2(Station station){   //Talkshow
     //screen.setLine(2, info);
 
     free(name_with_extension);
-    free(tmpString);
+    free(tmpString);*/
     //free(info);
+
+    int selectedTalk = random(4);   //Both talkshow stations only have 4 monos, so good
+
+    char name_with_extension[22];
+    strcpy(name_with_extension, station.source); 
+    
+
+    char tmpString[13];
+    sprintf(tmpString, "/MONO/%i.wav", selectedTalk);
+
+    strcat(name_with_extension, tmpString);
+
+    tmrpcm.play(name_with_extension, millis() / 1000);
+    screen.setLine(2, name_with_extension);
 }
 
 void handleStationChange(bool upOrDown){
+    delay(1000);    //This delay seems to fix some audio bugs with tmrpcm
     tmrpcm.loop(0);
     //tmrpcm.disable(); //Calling disable can sometimes stop play() from working correctly!
     tmrpcm.stopPlayback();
@@ -181,7 +235,8 @@ void loop()
 {
     if(!tmrpcm.isPlaying()){    //If no audio is playing, do something!
         //We could wrap this in a if(millis() % 1000 == 0), but isPlaying() is a very simple function that doesn't require any power, so no need (in fact wrapping it in the proposed function could be more intensive! A counter would be better)
-
+        //Only need to handle station types 0 and 1
+        
     }
 
     if(butNextStation.stateChanged()){
@@ -197,9 +252,10 @@ void loop()
         int volume = map(reading, 0, 1023, 0, 7);
         tmrpcm.setVolume(volume);
 
-        char* tmpString = new char[9];  //Needs to be 9
+        //char* tmpString = new char[9];  //Needs to be 9
+        char tmpString[9];
         sprintf(tmpString, "VOL :  %i", volume);
         screen.setLine(LCD_VOL_LINE, tmpString);
-        free(tmpString);
+        //free(tmpString);
     }
 }
