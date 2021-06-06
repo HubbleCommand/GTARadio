10bc453e7fe1b21266742dabd46d5c11d2cd1233
HubbleCommand <39958198+HubbleCommand@users.noreply.github.com>
2021-06-06 21:23:54 +0200

    MAIN USES STATION CLASSES

--- GTARadio/GTARadio.ino
+++ GTARadio/GTARadio.ino
@@ -1,74 +1,86 @@
 #include "src/Button/Button.h"
 #include "src/ScreenController/ScreenController.h"
+#include "src/StationAbstract/StationAbstract.h"
+#include "src/StationSplit/StationSplit.h"
+#include "src/StationUnsplit/StationUnsplit.h"
+#include "src/StationTalkshow/StationTalkshow.h"
 #include <Wire.h>
 #include <SPI.h>
 #include <SD.h>
 #include <TMRpcm.h>
 #include <avr/pgmspace.h>   //For PROGMEM
 #define SD_ChipSelectPin 4
+
+#if ((defined(__AVR_ATmega2560__) || defined(__AVR_ATmega2561__)))
+    #define TMRPCM_SPEAKER_PIN 11   //Use pin 11 on the MEGA
+#else
+    #define TMRPCM_SPEAKER_PIN 9    //Use pin 9  on the UNO
+#endif
+
 #define NUMBER_OF_STATIONS 17
 #define LCD_INTRO_LINE 0
 #define LCD_CHANNEL_LINE 1
 #define LCD_SONG_LINE 2
 #define LCD_VOL_LINE 3
-const char* src0 = "/SRC.wav";
-const int advCount PROGMEM = 96;
-const int newsCount PROGMEM = 107;
 
-//This is some ungodly code that could be made better, 
 //This is really only intended to be used with a Mega, as TMRpcm requires lots of space when dealing with high sample rate audio
 
 TMRpcm tmrpcm;
 ScreenController screen = ScreenController(0x27,20,4);    //0x27 for 20x4, 0x3f for 16x2
 Button butNextStation = Button(2);
 Button butPrevStation = Button(3);
+Button butNextSong = Button(40);
+Button butPrevSong = Button(41);
 
-//Defines data for each station
-//Is constant so can be stored in PROGMEM
-struct Station {
-    const char source [9]; //While it can be smaller, this is better for memory
-    const char* name;
-    char type; //0 - Unsplit, 1 - Split, 2 - Talkshow   //char is used instead of int as it is 1 instead of 2 bytes
-    char songCount;
-};
-
-//Stores the song & time reached for the song for a station
-//Will change so must be stored in SRAM
-struct StationPosition{
-    char songID;    //Playing song ID (the number)
-    int secs;     //The current time in seconds that the song last got to
-};
-
-//Storing these strings takes a lot of space, so put them in FLASH instead of SRAM. No other memory management to do as it is flash and is reset on each powerup.
-//TODO check wtf is going on here, only uses 2% of SRAM? Probably because struct uses pntrs, not the actual thing ? Still would take more space...
-const Station stations[NUMBER_OF_STATIONS] /*PROGMEM*/ = {
-    {"01_CROCK", "Los Santos Rock Radio", 1, 38},
-    {"02_POP", "Non Stop Pop", 1, 42},
-    {"03_HH_N", "Radio Los Santos", 1, 31},
-    {"04_PUNK", "Channel X", 1, 18},
-    {"05_T1", "West Coast Talk Radio", 2, 4},
-    {"06_CUNT", "Rebel Radio", 1, 17},
-    {"07_DAN1", "Soulwax FM", 0, 1},
-    {"08_MEX", "East Los FM", 0, 1},
-    {"09_HH_O", "West Coast Classics", 1, 29},
-    //NO No10
-    {"11_T2", "Blaine County Radio", 2, 4},
-    {"12_REGG", "Blue Ark FM", 1, 21},
-    {"13_JAZZ", "WorldWide FM", 0, 1},
-    {"14_DAN2", "FlyLo FM", 0, 1},
-    {"15_MTWN", "Low Down", 1, 20},
-    {"16_SILK", "Radio Mirror Park", 1, 34},
-    {"17_FUNK", "Space", 1, 21},
-    {"18_90RK", "Vinewood Boulevard Radio", 1, 20},
-};
-
-StationPosition stationSaves[NUMBER_OF_STATIONS] = {
-    {-1, -1},{-1, -1},{-1, -1},{-1, -1},{-1, -1},{-1, -1},{-1, -1},{-1, -1}
-};
+StationAbstract* stationsCLASS[NUMBER_OF_STATIONS]; //Needs to be array of pointers, as is Abstract class (would otherwise need to import std::vector)
 
 int selectedStationIndex;
-
 int lastPotState;
+int startTime = 0;
+
+StationSplit CLASSIC_ROCK = StationSplit((char*)"Los Santos Rock Radio", (char*)"01_CROCK", &tmrpcm, &screen);
+StationSplit NON_STOP_POP = StationSplit((char*)"Non Stop Pop", (char*)"02_POP", &tmrpcm, &screen);
+StationSplit RADIO_LOS_SANTOS = StationSplit((char*)"Radio Los Santos", (char*)"03_HH_N", &tmrpcm, &screen);
+StationSplit CHANNEL_X = StationSplit((char*)"Channel X", (char*)"04_PUNK", &tmrpcm, &screen);
+StationTalkshow WCTR = StationTalkshow((char*)"West Coast Talk Radio", (char*)"05_T1", &tmrpcm, &screen);  //TF?
+StationSplit REBEL_RADIO = StationSplit((char*)"Rebel Radio", (char*)"06_CUNT", &tmrpcm, &screen);
+StationUnsplit SOULWAX = StationUnsplit((char*)"Soulwax FM", (char*)"07_DAN1", &tmrpcm, &screen);
+StationUnsplit ELS = StationUnsplit((char*)"East Los FM", (char*)"08_MEX", &tmrpcm, &screen);
+StationSplit WCC = StationSplit((char*)"West Coast Classics", (char*)"09_HH_O", &tmrpcm, &screen);
+StationTalkshow BLAINE_CR = StationTalkshow((char*)"Blaine County Radio", (char*)"11_T2", &tmrpcm, &screen); //Plays, but screen doesn't work
+StationSplit BLUE_ARK = StationSplit((char*)"Blue Ark FM", (char*)"12_REGG", &tmrpcm, &screen);
+StationUnsplit WORLDWIDE = StationUnsplit((char*)"WorldWide FM", (char*)"13_JAZZ", &tmrpcm, &screen);
+StationUnsplit FLY_LO = StationUnsplit((char*)"FlyLo FM", (char*)"14_DAN2", &tmrpcm, &screen);
+StationSplit LOW_DOWN = StationSplit((char*)"Low Down", (char*)"15_MTWN", &tmrpcm, &screen);
+StationSplit MIRROR_PARK = StationSplit((char*)"Radio Mirror Park", (char*)"16_SILK", &tmrpcm, &screen);
+StationSplit SPACE = StationSplit((char*)"Space", (char*)"17_FUNK", &tmrpcm, &screen);
+StationSplit VB_RADIO = StationSplit((char*)"Vinewood Boulevard Radio", (char*)"18_90RK", &tmrpcm, &screen);
+
+//GTA IV stations
+//StationSplit AFRO_BEAT = StationSplit((char*)"IF99", (char*)"1_AFRO", &tmrpcm, &screen);
+//StationSplit TUFF_GONG = StationSplit((char*)"Tuff Gong Radio", (char*)"2_BBYLN", &tmrpcm, &screen);
+StationUnsplit THE_BEAT = StationUnsplit((char*)"The Beat 102.7", (char*)"GTAIV/3_BEAT", &tmrpcm, &screen);    //PREPPED
+StationUnsplit MASSIVE_B = StationUnsplit((char*)"Massive B Soundsystem 96.9", (char*)"GTAIV/4_BK", &tmrpcm, &screen); //PREPPED
+//StationSplit JOURNEY = StationSplit((char*)"The Journey", (char*)"5_CLASS", &tmrpcm, &screen);
+StationUnsplit ELECTRO_CHOC = StationUnsplit((char*)"Electro-Choc", (char*)"GTAIV/6_DANM", &tmrpcm, &screen);  //PREPPED
+//StationSplit RADIO_BROCKER = StationSplit((char*)"Radio Brocker", (char*)"7_DANR", &tmrpcm, &screen);
+//WTF 8? Just extra songs?
+StationSplit FUSION_FM = StationSplit((char*)"Fusion FM", (char*)"GTAIV/GTAIV/9_FUS", &tmrpcm, &screen);
+//StationSplit LC_HARD_CORE = StationSplit((char*)"Liberty City Hard Core", (char*)"10_HARD", &tmrpcm, &screen);
+//WTF 11? No songs?
+//StationSplit JAZZ_NATION = StationSplit((char*)"Jazz Nation Radio 108.5", (char*)"12_JAZZ", &tmrpcm, &screen);
+//StationSplit K109 = StationSplit((char*)"K109 The Studio", (char*)"13_K109", &tmrpcm, &screen);
+StationTalkshow INTEGRITY = StationTalkshow((char*)"Integrity 2.0", (char*)"GTAIV/14_LZLW", &tmrpcm, &screen); //PREPPED
+//StationSplit LIBERTY_ROCK = StationSplit((char*)"Liberty Rock Radio", (char*)"15_LIBR", &tmrpcm, &screen);
+//StationSplit ACTUALIZATION = StationSplit((char*)"Self-Actualization FM", (char*)"16_MED", &tmrpcm, &screen);
+StationUnsplit THE_CLASSICS = StationUnsplit((char*)"The Classics 104.1", (char*)"GTAIV/17_NYCL", &tmrpcm, &screen); //Not sure about name...  //PREPPED
+StationTalkshow PLR = StationTalkshow((char*)"Public Liberty Radio", (char*)"GTAIV/18_PLR", &tmrpcm, &screen); //PREPED
+StationUnsplit RAMJAM = StationUnsplit((char*)"RamJam FM", (char*)"GTAIV/19_RJFM", &tmrpcm, &screen);  //PREPED
+//StationSplit SAN_JUAN_SOUNDS = StationSplit((char*)"San Juan Sounds", (char*)"20_SJS", &tmrpcm, &screen);
+//StationSplit THE_VIBE = StationSplit((char*)"The Vibe 98.8", (char*)"21_VIBE", &tmrpcm, &screen);
+//StationSplit VCFM = StationSplit((char*)"Vice City FM", (char*)"22_VCFM", &tmrpcm, &screen);
+//StationSplit VLADIVOSTOK = StationSplit((char*)"Vladivostok FM", (char*)"23_VLAD", &tmrpcm, &screen);
+StationTalkshow WKTT = StationTalkshow((char*)"WKTT Radio", (char*)"GTAIV/23_WKTT", &tmrpcm, &screen); //PREPED
 
 void setup()
 {
@@ -89,143 +101,70 @@ void setup()
     tmrpcm.volume(0.5);
     //tmrpcm.quality(1);
     selectedStationIndex = random(NUMBER_OF_STATIONS);   //DON't -1 ! the max is EXCLUSIVE
-    handleStationChange(0);
     
+    stationsCLASS[0] = &CLASSIC_ROCK;
+    stationsCLASS[1] = &NON_STOP_POP;
+    stationsCLASS[2] = &RADIO_LOS_SANTOS;
+    stationsCLASS[3] = &CHANNEL_X;
+    stationsCLASS[4] = &WCTR;
+    stationsCLASS[5] = &REBEL_RADIO;
+    stationsCLASS[6] = &SOULWAX;
+    stationsCLASS[7] = &ELS;
+    stationsCLASS[8] = &WCC;
+    stationsCLASS[9] = &BLAINE_CR;
+    stationsCLASS[10] = &BLUE_ARK;
+    stationsCLASS[11] = &WORLDWIDE;
+    stationsCLASS[12] = &FLY_LO;
+    stationsCLASS[13] = &LOW_DOWN;
+    stationsCLASS[14] = &MIRROR_PARK;
+    stationsCLASS[15] = &SPACE;
+    stationsCLASS[16] = &VB_RADIO;
+
+    //Not updating the screen pointer AFTER THE SCREEN HAS BEEN SETUP causes issues
+    for(int i = 0; i < NUMBER_OF_STATIONS; i++){
+        stationsCLASS[i]->screen = &screen;
+    }
+
+    selectedStationIndex = 0;
+    stationsCLASS[0]->play();
 }
 
-void playAd(){
-    char nameAd[13];
-    int selAd = random(96); //There are a total of 96 ads, so generate a number to get
-    sprintf(nameAd, "ADS/%i.wav", selAd);
-    tmrpcm.play(nameAd);
-}
+void loop()
+{    
+    if(butNextStation.stateChanged()){
+        stationsCLASS[selectedStationIndex]->stop();
 
-void playNews(){
-    char nameNews[14];
-    int selNews = random(107); //There are a total of 107 news lines, so generate a number to get
-    sprintf(nameNews, "NEWS/%i.wav", selNews);
-    tmrpcm.play(nameNews);
-}
-
-void startType0(Station station){   //Unsplit
-    screen.setLine(1, (char*)station.name);
-    char nameUnsplit[20];
-    strcpy(nameUnsplit, station.source); 
-    strcat(nameUnsplit, src0);
-    tmrpcm.play(nameUnsplit, millis() / 1000);
-    tmrpcm.loop(1);
-}
-
-void startType1(Station station){   //Split
-    screen.setLine(1, station.name);
-    int selSong = random(station.songCount - '0');
-    char namelov[22];
-    strcpy(namelov, station.source);
-    
-    char tString[14];
-    sprintf(tString, "/SONGS/%i.wav", selSong);
-
-    strcat(namelov, tString);
-    screen.setLine(2, namelov);
-
-    tmrpcm.play(namelov);
-}
-
-void startType2(Station station){   //Talkshow
-    screen.setLine(1, (char*)station.name);
-
-    //TODO printing info causes a crash
-    //screen.setLine(2, name_with_extension);
-
-    //char info[32];
-	//tmrpcm.listInfo((char*)"RAIN.wav",info,0);
-    //tmrpcm.listInfo(name_with_extension,info,0);
-    //screen.setLine(2, info);
-
-    int selectedTalk = random(4);   //Both talkshow stations only have 4 monos, so good
-
-    char name_with_extension[22];
-    strcpy(name_with_extension, station.source); 
-    
-
-    char tmpString[13];
-    sprintf(tmpString, "/MONO/%i.wav", selectedTalk);
-
-    strcat(name_with_extension, tmpString);
-
-    tmrpcm.play(name_with_extension, millis() / 1000);
-    screen.setLine(2, name_with_extension);
-}
-
-int startTime = 0;
-
-void handleStationChange(bool upOrDown){
-    delay(1000);    //This delay seems to fix some audio bugs with tmrpcm
-    tmrpcm.loop(0);
-    tmrpcm.stopPlayback();
-    
-    if(upOrDown){
         if(selectedStationIndex >= NUMBER_OF_STATIONS - 1){
             selectedStationIndex = 0;
         } else {
             selectedStationIndex++;
         }
         
-    } else {
+        stationsCLASS[selectedStationIndex]->play();
+    }
+
+    if(butPrevStation.stateChanged()){
+        stationsCLASS[selectedStationIndex]->stop();
+        
         if(selectedStationIndex <= 0){
             selectedStationIndex = NUMBER_OF_STATIONS - 1;
         } else {
             selectedStationIndex--;
         }
+
+        stationsCLASS[selectedStationIndex]->play();
     }
 
-    Station selectedStation = stations[selectedStationIndex];
-    StationPosition selectedStationPosition = stationSaves[selectedStationIndex];
-    selectedStationPosition.secs = millis() - startTime;
-    stationSaves[selectedStationIndex] = selectedStationPosition;
-
-    switch(selectedStation.type){
-        case 0:
-            startType0(selectedStation);
-            break;
-        case 1:
-            startType1(selectedStation);
-            break;
-        case 2:
-            startType2(selectedStation);
-            break;
-    }
-}
-
-void loop()
-{
-    if(!tmrpcm.isPlaying()){    //If no audio is playing, do something!
-        //We could wrap this in a if(millis() % 1000 == 0), but isPlaying() is a very simple function that doesn't require any power, so no need (in fact wrapping it in the proposed function could be more intensive! A counter would be better)
-        //Only need to handle station types 0 and 1
-        Station selectedStation = stations[selectedStationIndex];
-
-        switch(selectedStation.type){
-            case 0:
-                startType0(selectedStation);
-                break;
-            case 1:
-                startType1(selectedStation);
-                break;
-            case 2:
-                startType2(selectedStation);
-                break;
-        }
+    if(butNextSong.stateChanged()){
+        stationsCLASS[selectedStationIndex]->nextSong();
     }
 
-    if(butNextStation.stateChanged()){
-        handleStationChange(true);
-    }
-    if(butPrevStation.stateChanged()){
-        handleStationChange(false);
+    if(butPrevSong.stateChanged()){
+        stationsCLASS[selectedStationIndex]->prevSong();
     }
 
     int reading = analogRead(A0);
-    if(abs(reading - lastPotState) > 10){
+    if(abs(reading - lastPotState) > 50){
         lastPotState = reading;
         int volume = map(reading, 0, 1023, 0, 7);
         tmrpcm.setVolume(volume);
@@ -233,5 +172,9 @@ void loop()
         char tmpString[9];
         sprintf(tmpString, "VOL :  %i", volume);
         screen.setLine(LCD_VOL_LINE, tmpString);
+    }
+
+    if(!tmrpcm.isPlaying()){    //If no audio is playing, do something!
+        stationsCLASS[selectedStationIndex]->cont();
     }
 }
