b289c8f6c0b99e5e386eb8037e20b9754ca01315
HubbleCommand <39958198+HubbleCommand@users.noreply.github.com>
2021-06-08 22:47:02 +0200

    !!! Main all stations audio source working !!!

--- GTARadio/GTARadio.ino
+++ GTARadio/GTARadio.ino
@@ -17,7 +17,7 @@
     #define TMRPCM_SPEAKER_PIN 9    //Use pin 9  on the UNO
 #endif
 
-#define NUMBER_OF_STATIONS 17
+#define NUMBER_OF_STATIONS 39
 #define LCD_INTRO_LINE 0
 #define LCD_CHANNEL_LINE 1
 #define LCD_SONG_LINE 2
@@ -42,12 +42,12 @@ StationSplit CLASSIC_ROCK = StationSplit((char*)"Los Santos Rock Radio", (char*)
 StationSplit NON_STOP_POP = StationSplit((char*)"Non Stop Pop", (char*)"02_POP", &tmrpcm, &screen);
 StationSplit RADIO_LOS_SANTOS = StationSplit((char*)"Radio Los Santos", (char*)"03_HH_N", &tmrpcm, &screen);
 StationSplit CHANNEL_X = StationSplit((char*)"Channel X", (char*)"04_PUNK", &tmrpcm, &screen);
-StationTalkshow WCTR = StationTalkshow((char*)"West Coast Talk Radio", (char*)"05_T1", &tmrpcm, &screen);  //TF?
+StationTalkshow WCTR = StationTalkshow((char*)"West Coast Talk Radio", (char*)"05_T1", &tmrpcm, &screen);
 StationSplit REBEL_RADIO = StationSplit((char*)"Rebel Radio", (char*)"06_CUNT", &tmrpcm, &screen);
 StationUnsplit SOULWAX = StationUnsplit((char*)"Soulwax FM", (char*)"07_DAN1", &tmrpcm, &screen);
 StationUnsplit ELS = StationUnsplit((char*)"East Los FM", (char*)"08_MEX", &tmrpcm, &screen);
 StationSplit WCC = StationSplit((char*)"West Coast Classics", (char*)"09_HH_O", &tmrpcm, &screen);
-StationTalkshow BLAINE_CR = StationTalkshow((char*)"Blaine County Radio", (char*)"11_T2", &tmrpcm, &screen); //Plays, but screen doesn't work
+StationTalkshow BLAINE_CR = StationTalkshow((char*)"Blaine County Radio", (char*)"11_T2", &tmrpcm, &screen);
 StationSplit BLUE_ARK = StationSplit((char*)"Blue Ark FM", (char*)"12_REGG", &tmrpcm, &screen);
 StationUnsplit WORLDWIDE = StationUnsplit((char*)"WorldWide FM", (char*)"13_JAZZ", &tmrpcm, &screen);
 StationUnsplit FLY_LO = StationUnsplit((char*)"FlyLo FM", (char*)"14_DAN2", &tmrpcm, &screen);
@@ -57,30 +57,30 @@ StationSplit SPACE = StationSplit((char*)"Space", (char*)"17_FUNK", &tmrpcm, &sc
 StationSplit VB_RADIO = StationSplit((char*)"Vinewood Boulevard Radio", (char*)"18_90RK", &tmrpcm, &screen);
 
 //GTA IV stations
-//StationSplit AFRO_BEAT = StationSplit((char*)"IF99", (char*)"1_AFRO", &tmrpcm, &screen);
-//StationSplit TUFF_GONG = StationSplit((char*)"Tuff Gong Radio", (char*)"2_BBYLN", &tmrpcm, &screen);
-StationUnsplit THE_BEAT = StationUnsplit((char*)"The Beat 102.7", (char*)"GTAIV/3_BEAT", &tmrpcm, &screen);    //PREPPED
-StationUnsplit MASSIVE_B = StationUnsplit((char*)"Massive B Soundsystem 96.9", (char*)"GTAIV/4_BK", &tmrpcm, &screen); //PREPPED
-//StationSplit JOURNEY = StationSplit((char*)"The Journey", (char*)"5_CLASS", &tmrpcm, &screen);
-StationUnsplit ELECTRO_CHOC = StationUnsplit((char*)"Electro-Choc", (char*)"GTAIV/6_DANM", &tmrpcm, &screen);  //PREPPED
-//StationSplit RADIO_BROCKER = StationSplit((char*)"Radio Brocker", (char*)"7_DANR", &tmrpcm, &screen);
+StationSplit IF_99 = StationSplit((char*)"International Funk 99", (char*)"A_AFRO", &tmrpcm, &screen);
+StationSplit TUFF_GONG = StationSplit((char*)"Tuff Gong Radio", (char*)"B_BBYLN", &tmrpcm, &screen);
+StationUnsplit THE_BEAT = StationUnsplit((char*)"The Beat 102.7", (char*)"C_BEAT", &tmrpcm, &screen);
+StationUnsplit MASSIVE_B = StationUnsplit((char*)"Massive B Soundsystem 96.9", (char*)"D_BK", &tmrpcm, &screen);
+StationSplit JOURNEY = StationSplit((char*)"The Journey", (char*)"E_CLASS", &tmrpcm, &screen);
+StationUnsplit ELECTRO_CHOC = StationUnsplit((char*)"Electro-Choc", (char*)"F_DANM", &tmrpcm, &screen);
+StationSplit RADIO_BROCKER = StationSplit((char*)"Radio Brocker", (char*)"G_DANR", &tmrpcm, &screen);
 //WTF 8? Just extra songs?
-StationSplit FUSION_FM = StationSplit((char*)"Fusion FM", (char*)"GTAIV/GTAIV/9_FUS", &tmrpcm, &screen);
-//StationSplit LC_HARD_CORE = StationSplit((char*)"Liberty City Hard Core", (char*)"10_HARD", &tmrpcm, &screen);
-//WTF 11? No songs?
-//StationSplit JAZZ_NATION = StationSplit((char*)"Jazz Nation Radio 108.5", (char*)"12_JAZZ", &tmrpcm, &screen);
-//StationSplit K109 = StationSplit((char*)"K109 The Studio", (char*)"13_K109", &tmrpcm, &screen);
-StationTalkshow INTEGRITY = StationTalkshow((char*)"Integrity 2.0", (char*)"GTAIV/14_LZLW", &tmrpcm, &screen); //PREPPED
-//StationSplit LIBERTY_ROCK = StationSplit((char*)"Liberty Rock Radio", (char*)"15_LIBR", &tmrpcm, &screen);
-//StationSplit ACTUALIZATION = StationSplit((char*)"Self-Actualization FM", (char*)"16_MED", &tmrpcm, &screen);
-StationUnsplit THE_CLASSICS = StationUnsplit((char*)"The Classics 104.1", (char*)"GTAIV/17_NYCL", &tmrpcm, &screen); //Not sure about name...  //PREPPED
-StationTalkshow PLR = StationTalkshow((char*)"Public Liberty Radio", (char*)"GTAIV/18_PLR", &tmrpcm, &screen); //PREPED
-StationUnsplit RAMJAM = StationUnsplit((char*)"RamJam FM", (char*)"GTAIV/19_RJFM", &tmrpcm, &screen);  //PREPED
-//StationSplit SAN_JUAN_SOUNDS = StationSplit((char*)"San Juan Sounds", (char*)"20_SJS", &tmrpcm, &screen);
-//StationSplit THE_VIBE = StationSplit((char*)"The Vibe 98.8", (char*)"21_VIBE", &tmrpcm, &screen);
-//StationSplit VCFM = StationSplit((char*)"Vice City FM", (char*)"22_VCFM", &tmrpcm, &screen);
-//StationSplit VLADIVOSTOK = StationSplit((char*)"Vladivostok FM", (char*)"23_VLAD", &tmrpcm, &screen);
-StationTalkshow WKTT = StationTalkshow((char*)"WKTT Radio", (char*)"GTAIV/23_WKTT", &tmrpcm, &screen); //PREPED
+StationSplit FUSION_FM = StationSplit((char*)"Fusion FM", (char*)"H_FUS", &tmrpcm, &screen);    //Not working?
+StationSplit LC_HARD_CORE = StationSplit((char*)"Liberty City Hard Core", (char*)"I_HARD", &tmrpcm, &screen);
+//WTF 11? No songs? !!! CHANNEL 11 WAS MEANT FOR A USER-MADE RADIO STATION ON PC VERSIONS OF GTA IV !!!
+StationSplit JAZZ_NATION = StationSplit((char*)"Jazz Nation Radio 108.5", (char*)"J_JAZZ", &tmrpcm, &screen);
+StationSplit K109 = StationSplit((char*)"K109 The Studio", (char*)"K_K109", &tmrpcm, &screen);
+StationTalkshow INTEGRITY = StationTalkshow((char*)"Integrity 2.0", (char*)"L_LZLW", &tmrpcm, &screen);
+StationSplit LIBERTY_ROCK = StationSplit((char*)"Liberty Rock Radio", (char*)"M_LIBR", &tmrpcm, &screen);
+StationSplit ACTUALIZATION = StationSplit((char*)"Self-Actualization FM", (char*)"N_MED", &tmrpcm, &screen);
+StationUnsplit THE_CLASSICS = StationUnsplit((char*)"The Classics 104.1", (char*)"O_NYCL", &tmrpcm, &screen); //Not sure about name...
+StationTalkshow PLR = StationTalkshow((char*)"Public Liberty Radio", (char*)"P_PLR", &tmrpcm, &screen);
+StationUnsplit RAMJAM = StationUnsplit((char*)"RamJam FM", (char*)"Q_RJFM", &tmrpcm, &screen);
+StationSplit SAN_JUAN_SOUNDS = StationSplit((char*)"San Juan Sounds", (char*)"R_SJS", &tmrpcm, &screen);
+StationSplit THE_VIBE = StationSplit((char*)"The Vibe 98.8", (char*)"S_VIBE", &tmrpcm, &screen);
+StationSplit VCFM = StationSplit((char*)"Vice City FM", (char*)"T_VCFM", &tmrpcm, &screen);
+StationSplit VLADIVOSTOK = StationSplit((char*)"Vladivostok FM", (char*)"U_VLAD", &tmrpcm, &screen);
+StationTalkshow WKTT = StationTalkshow((char*)"WKTT Radio", (char*)"V_WKTT", &tmrpcm, &screen);
 
 void setup()
 {
@@ -119,6 +119,46 @@ void setup()
     stationsCLASS[14] = &MIRROR_PARK;
     stationsCLASS[15] = &SPACE;
     stationsCLASS[16] = &VB_RADIO;
+    stationsCLASS[17] = &IF_99;
+    stationsCLASS[18] = &TUFF_GONG;
+    stationsCLASS[19] = &THE_BEAT;
+
+    //stationsCLASS[19] = &MASSIVE_B;
+    stationsCLASS[20] = &MASSIVE_B;
+
+    stationsCLASS[21] = &JOURNEY;
+
+    //stationsCLASS[20] = &ELECTRO_CHOC;
+    stationsCLASS[22] = &ELECTRO_CHOC;
+
+    stationsCLASS[23] = &RADIO_BROCKER;
+    stationsCLASS[24] = &FUSION_FM;
+    stationsCLASS[25] = &LC_HARD_CORE;
+    stationsCLASS[26] = &JAZZ_NATION;
+    stationsCLASS[27] = &K109;
+
+    //stationsCLASS[21] = &INTEGRITY;
+    stationsCLASS[28] = &INTEGRITY;
+
+    stationsCLASS[29] = &LIBERTY_ROCK;
+    stationsCLASS[30] = &ACTUALIZATION;
+
+    //stationsCLASS[22] = &THE_CLASSICS;
+    stationsCLASS[31] = &THE_CLASSICS;
+
+    //stationsCLASS[23] = &PLR;
+    stationsCLASS[32] = &PLR;
+
+    //stationsCLASS[24] = &RAMJAM;
+    stationsCLASS[33] = &RAMJAM;
+
+    stationsCLASS[34] = &SAN_JUAN_SOUNDS;
+    stationsCLASS[35] = &THE_VIBE;
+    stationsCLASS[36] = &VCFM;
+    stationsCLASS[37] = &VLADIVOSTOK;
+
+    //stationsCLASS[25] = &WKTT;
+    stationsCLASS[38] = &WKTT;
 
     //Not updating the screen pointer AFTER THE SCREEN HAS BEEN SETUP causes issues
     for(int i = 0; i < NUMBER_OF_STATIONS; i++){
